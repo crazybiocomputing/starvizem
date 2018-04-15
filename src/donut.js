f7e471b69c5fceb1f06ff706585616b61edfb112
Bock Pauline <33928467+PaulineBock@users.noreply.github.com>
2018-04-15 11:01:06 +0200

    Update donut.js

--- src/donut.js
+++ src/donut.js
@@ -27,7 +27,7 @@
 /**
  * Create a Donut in svg and returns it
  *
- * @author Marie
+ * @author Marie Economides, Pauline Bock
  * 
  * TODO : add interactive labels, virer les classes trop petites ou les zoomer ... 
  */
@@ -37,6 +37,12 @@ function createDonut(data, width, height) {
         .attr("width", width)
         .attr("height", height)
         .style("border", "2px solid rgba(2, 0, 34, 0.897");
+    
+    svg.append("g")
+    .attr("class", "labelName");
+
+    svg.append("g")
+    .attr("class", "labelValue");
 
     let radius = Math.min(width, height) / 2,
         innerRadius = radius * 0.5,
@@ -51,14 +57,18 @@ function createDonut(data, width, height) {
 
     let arcFinal = d3.arc().innerRadius(innerRadiusFinal).outerRadius(radius - 10);
     let arcZoom = d3.arc().innerRadius(innerRadiusZoom).outerRadius(radius - 10);
+    
+    let legendBoxSize = (radius * 0.05);
+    let legendSpacing = radius * 0.02;
 
+    let div = d3.select("body").append("div").attr("class", "toolTip");
 
     let labelArc = d3.arc()
         .outerRadius(radius - 40)
         .innerRadius(radius - 40);
 
     let pie = d3.pie()
-        .value(function(d) { return d.nb; });
+        .sort(d3.scaleOrdinal()).value(function(d) { return d.nb; });
 
     let g = svg.append("g")
         .attr("class", "arc")
@@ -67,12 +77,42 @@ function createDonut(data, width, height) {
         .enter().append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
         .on("mouseover", mouseover)
-        .on("mouseout", mouseout);
+        .on("mouseout", mouseout)
+        .on("mousemove", function(d){
+            div.style("left", d3.event.pageX+10+"px");
+            div.style("top", d3.event.pageY-25+"px");
+            div.style("display", "inline-block");
+            div.html("Class "+(d.data.labels)+"<br>"+(d.data.nb)+" images");
+        });
 
     g.append("path")
         .attr("fill", function(d, i) { return color(i); })
         .attr("d", arc)
         .attr("stroke", "#fff");
+    
+    let legend = svg.selectAll(".legend")
+        .data(color.domain())
+        .enter()
+        .append("g")
+        .attr("class", "legend")
+        .attr("transform", function (d,i) {
+            let h = legendBoxSize + legendSpacing;
+            let offset = h * color.domain().length / 2;
+            let horiz = -3 * legendBoxSize;
+            let vert = i * h - offset;
+            return "translate(" + horiz + "," + vert + ")";
+        });
+
+    legend.append("rect")
+        .attr("width", legendBoxSize)
+        .attr("height", legendBoxSize)
+        .style("fill", color)
+        .style("stroke", color);
+
+    legend.append("text")
+        .attr("x", legendBoxSize + legendSpacing)
+        .attr("y", legendBoxSize - legendSpacing)
+        .text(function(d){return d;});
 
     function mouseover() {
         d3.select(this).select("path").transition()
