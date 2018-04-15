de2012a904a2869ebfeddb9cdf1a0cb2c64b167e
kaleapoholi <32905372+kaleapoholi@users.noreply.github.com>
2018-04-16 00:55:13 +0200

    Update arcDiagram.js
    liens a verifier target undefined ...

--- src/arcDiagram.js
+++ src/arcDiagram.js
@@ -35,77 +35,120 @@
  */
 
 function createArcDiagram(data, width, height) {
+    //create svg
     let svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height)
         .style("border", "2px solid rgba(2, 0, 34, 0.897");
 
     let color = d3.scaleOrdinal(d3.schemeAccent);
+    let datanode = data.node;
+    let datalink = data.links;
+
+    let pad = 30 * height / 100; // actual padding amount
+    let radius = 10; // fixed node radius
+    let yfixed = pad + radius; // y position for all nodes
+
+    // fix graph links to map to objects instead of indices
+    datalink.forEach(function(d, i) {
+        d.source = isNaN(d.source) ? d.source : datanode[d.source];
+        d.target = isNaN(d.target) ? d.target : datanode[d.target];
+    });
+
+    datanode.sort(function(a, b) {
+        return a.id - b.id;
+    });
+
+    let x = d3.scaleLinear().range([0, 90 * width / 100]);
+    x.domain([0, d3.max(datanode, function(d) { return d.id; }) - 1]);
+
+    // calculate pixel location for each node
+    datanode.forEach(function(d, i) {
+        d.x = x(i);
+        d.y = yfixed;
+    });
+
+    let nodeRadius = d3.scaleSqrt().range([5, 20]);
+    let linkWidth = d3.scaleLinear().range([1.5, 2 * nodeRadius.range()[0]]);
+
+    nodeRadius.domain(d3.extent(datanode, function(d) {
+        return d.value;
+    }));
+
+    linkWidth.domain(d3.extent(datalink, function(d) {
+        return d.value;
+    }));
 
     let node = svg.append("g")
-        .attr("class", "jobs")
-        .selectAll("g")
-        .data(data.jobs)
-        .enter().append("g");
-
-    let circle = node.append("circle")
-        .attr("r", 8)
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(datanode)
+        .enter().append("circle")
+        .attr("r", function(d) {
+            return nodeRadius(d.value)
+        })
+        .attr("cx", function(d) {
+            return x(d.id)
+        })
+        .attr("cy", 300)
         .attr("fill", function(d) { return color(d.process); });
 
-    let labels = node.append("text")
+    let labels = svg.append("g")
+        .attr("class", "text")
+        .selectAll("text")
+        .data(datanode)
+        .enter().append("text")
         .text(function(d) {
-            return "job" + d.jobID;
+            return "job" + d.id;
         })
-        .attr('x', -2)
-        .attr('y', 8)
+        .attr('x', function(d) {
+            return x(d.id)
+        })
+        .attr('y', 2.5 * yfixed + 10)
         .style("writing-mode", "vertical-rl")
         .style("text-orientation", "sideways")
         .style("fill", "black")
-        .style("font-size", "10px")
+        .style("font-size", "14px")
         .style("font-family", "Arial");
 
-    let link = svg.append("g")
-        .selectAll("link")
-        .data(data.jobs)
-        .append("line")
+    /*
+    // scale to generate radians (just for lower-half of circle)
+    let radians = d3.scaleLinear()
+        .range([Math.PI / 2, 3 * Math.PI / 2]);
+
+    // path generator for arcs (uses polar coordinates)
+    let arc = d3.radialLine()
+        .angle(function(d) { return radians(d); });
+
+    let links = svg.append("g").selectAll("link")
+        .data(datalink)
+        .enter()
+        .append("path")
         .attr("class", "link")
-        .enter().append("g");
+        .attr("transform", function(d, i) {
+            let xshift = d.source.x + (d.target.x - d.source.x) / 2;
+            let yshift = yfixed;
+            return "translate(" + xshift + ", " + yshift + ")";
+        })
+        .attr("d", function(d, i) {
+            // get x distance between source and target
+            let xdist = Math.abs(d.source.x - d.target.x);
+            // set arc radius based on x distance
+            arc.radius(xdist / 2);
+            // want to generate 1/3 as many points per pixel in x direction
+            let points = d3.range(0, Math.ceil(xdist / 3));
+            // set radian scale domain
+            radians.domain([0, points.length - 1]);
+            return arc(points);
+        })
+        .style("fill", color)
+        .style("stroke", color);
+*/
 
-    let simulation = d3.forceSimulation()
-        .force("charge", d3.forceManyBody())
-        //.force("link", d3.forceLink().id(function(d) { return d.jobs }))
-        //.force("collide", d3.forceCollide().radius(12))
-        .force("center", d3.forceCenter(width / 2, height / 2));
 
-
-    simulation
-        .nodes(data.jobs)
-        //.force("link").links(data.jobs)
-        .on("tick", ticked);
-
-
-    function ticked() {
-        node
-            .attr("transform", function(d) {
-                return "translate(" + d.x + "," + 200 + ")";
-            })
-
-        link.attr("x1", function(d) {
-                return d.source.x;
-            })
-            .attr("y1", function(d) {
-                return d.source.y;
-            })
-            .attr("x2", function(d) {
-                return d.target.x;
-            })
-            .attr("y2", function(d) {
-                return d.target.y;
-            });
-
-    }
 
     return svg.node();
+
 }
 
 
