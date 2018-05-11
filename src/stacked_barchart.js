70cc1bce52496a26bd7c703d9d248460298757ba
Bock Pauline <33928467+PaulineBock@users.noreply.github.com>
2018-05-11 13:53:00 +0200

    Update stacked_barchart.js

--- src/stacked_barchart.js
+++ src/stacked_barchart.js
@@ -25,7 +25,7 @@
 
 'use strict';
 
-function createStackedBarChart(data, width, height) {
+function createStackedBarChart(job, data, width, height) {
     // constants declaration
     let padding = 0.5;
     let cutClassName = 13;
@@ -140,10 +140,10 @@ function createStackedBarChart(data, width, height) {
         tooltip.style("top", d3.event.pageY-25+"px");
         tooltip.style("display", "inline-block");
         tooltip.html((d[1]-d[0])+" images");
-      });
-      /*.on("click", function (d) {
+      })
+      .on("click", function (d) {
         displayImage(job, d.data.name);
-      });*/
+      });
 
     //create x axis
     g.append("g")
