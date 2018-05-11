3200bc6b2b982efb8afbbe60452e44efda783aee
Bock Pauline <33928467+PaulineBock@users.noreply.github.com>
2018-05-11 13:47:21 +0200

    Fix bugs

--- src/donut.js
+++ src/donut.js
@@ -79,8 +79,8 @@ function createDonut(data, width, height) {
             tooltip.style("top", d3.event.pageY-25+"px");
             tooltip.style("display", "inline-block");
             tooltip.html("Class "+(d.data.labels)+"<br>"+(d.data.nb)+" images");
-        })
-        .on("click", function (d) {
+        });
+        //.on("click", function (d) {
             displayImage(job, d.data.labels);
         });
 
