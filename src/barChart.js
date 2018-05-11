49ac8ec682317c6f1e4485af7b602cf3933cb595
GuillaumeSotton <33928461+GuillaumeSotton@users.noreply.github.com>
2018-05-11 16:02:52 +0200

    Update barChart.js

--- src/barChart.js
+++ src/barChart.js
@@ -26,14 +26,16 @@
 'use strict';
 
 function createBarChart(data, width, height) {
+    //SVG creation and its properties
     let svg = d3.create("svg")
         .attr("width", width)
         .attr("height", height)
         .style("border", "2px solid rgba(2, 0, 34, 0.897");
 
+    //Color table
     let color = d3.scaleOrdinal(d3.schemeAccent);
 
-    //axis
+    //Axis x and y
     let x = d3.scaleLinear()
         .range([0, 90 * width / 100]);
 
@@ -41,12 +43,14 @@ function createBarChart(data, width, height) {
         .range([90 * height / 100, 40])
         .padding(0.1);
 
+    //Sort the data
     data.sort(function(a, b) { return a.nb - b.nb; });
 
+    //Domain of X and Y axis
     x.domain([0, d3.max(data, function(d) { return d.nb })]);
     y.domain(data.map(function(d) { return d.labels }));
 
-
+    //Bar creation
     svg.selectAll(".bar")
         .data(data)
         .enter().append("rect")
@@ -57,6 +61,7 @@ function createBarChart(data, width, height) {
         .attr("y", function(d) { return y(d.labels); })
         .attr("height", y.bandwidth());
 
+    //Add and  calibrate X and Y axis
     svg.append("g")
         .attr("transform", "translate(" + 9 * width / 100 + ",0)")
         .call(d3.axisLeft(y));
